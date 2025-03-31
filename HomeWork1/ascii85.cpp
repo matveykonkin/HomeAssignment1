91465767d0315d4dc30f44534146ec53a3b36fa9
Konkin Matvey <st135668@student.spbu.ru>
2025-03-31 19:38:41 +0300

    Decoding from ASCII85

--- HomeWork1/ascii85.cpp
+++ HomeWork1/ascii85.cpp
@@ -37,3 +37,50 @@ std::string encodeAscii85(const std::vector<unsigned char>& data) {
     
     return result;
 }
+
+std::vector<unsigned char> decodeAscii85(const std::string& input) {
+    std::vector<unsigned char> result;
+    size_t i = 0;
+    
+    while (i < input.size()) {
+        if (isspace(input[i])) {
+            i++;
+            continue;
+        }
+        
+        if (input[i] == 'z') {
+            for (int j = 0; j < 4; j++) {
+                result.push_back(0);
+            }
+            i++;
+            continue;
+        }
+        
+        uint32_t value = 0;
+        int chars = 0;
+        
+        for (; chars < 5 && i + chars < input.size(); chars++) {
+            char c = input[i + chars];
+            
+            if (c < '!' || c > 'u') {
+                throw std::runtime_error("Invalid ASCII85 character");
+            }
+            
+            value = value * 85 + (c - '!');
+        }
+        
+        i += chars;
+        
+        if (chars < 5) {
+            value *= pow(85, 5 - chars);
+        }
+        
+        for (int j = 3; j >= 0; j--) {
+            if (j < chars - 1) {
+                result.push_back((value >> (8 * j)) & 0xFF);
+            }
+        }
+    }
+    
+    return result;
+}
